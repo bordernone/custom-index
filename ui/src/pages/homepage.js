ff99d08a589883db65877fc3b7c4af46399e4eb2
bishnudev <devbishnu2000@gmail.com>
2022-02-20 21:12:22 +0400

    Added warning for incomplete proportion

--- ui/src/pages/homepage.js
+++ ui/src/pages/homepage.js
@@ -1,8 +1,8 @@
-import {Button, Container, Form, Table, Row, Col} from 'react-bootstrap';
+import {Button, Container, Form, Table, Row, Col, Badge, Overlay, Tooltip} from 'react-bootstrap';
 import './homepage.css';
-import {useEffect, useState} from "react";
+import {useEffect, useRef, useState} from "react";
 import axios from "axios";
-import { useNavigate } from "react-router";
+import {useNavigate} from "react-router";
 import LoadingOverlay from 'react-loading-overlay';
 import DatePicker from 'react-datepicker';
 import "react-datepicker/dist/react-datepicker.css";
@@ -67,6 +67,8 @@ const TableRow = ({updateHandler}) => {
 function TableInput({onSubmit}) {
     const [elementsProp, setElementsProp] = useState([]);
     const [numberOfElements, setNumberOfElements] = useState(0);
+    const proportionWarningTarget = useRef(null);
+    const [showProportionWarning, setShowProportionWarning] = useState(false);
 
     const onElementUpdate = (rowIndex, newObj) => {
         let elemProps = [...elementsProp];
@@ -82,6 +84,15 @@ function TableInput({onSubmit}) {
         setNumberOfElements(numberOfElements + 1);
     }
 
+    const getTotalProportion = () => {
+        if (elementsProp.length === 0) return 0;
+        let total = 0;
+        elementsProp.forEach(item => {
+            total = total + parseInt(item.proportion);
+        })
+        return total;
+    }
+
     return (
         <div>
             <Row>
@@ -90,25 +101,46 @@ function TableInput({onSubmit}) {
                     <tr>
                         <th className={"width-200"}>Instrument</th>
                         <th className={"width-200"}>Description</th>
-                        <th className={"width-200"}>Investment Proportion (%)</th>
+                        <th className={"width-200"}>Investment Proportion (%)
+                            <Badge className={(getTotalProportion() === 100)?"proportion-badge-complete":"proportion-badge-incomplete"} ref={proportionWarningTarget} >
+                                {getTotalProportion()}/100
+                            </Badge>
+
+                            <Overlay target={proportionWarningTarget.current} show={showProportionWarning && (getTotalProportion() !== 100)} placement="top">
+                                {(props) => (
+                                    <Tooltip className="warning-overlay" {...props}>
+                                        This should add up to 100.
+                                    </Tooltip>
+                                )}
+                            </Overlay>
+                        </th>
                     </tr>
                     </thead>
                     <tbody>
                     {
-                        new Array(numberOfElements).fill(0).map((item, index) => <TableRow key={index} updateHandler={(newObj) => {onElementUpdate(index, newObj)}}/>)
+                        new Array(numberOfElements).fill(0).map((item, index) => <TableRow key={index}
+                                                                                           updateHandler={(newObj) => {
+                                                                                               onElementUpdate(index, newObj)
+                                                                                           }}/>)
                     }
                     </tbody>
                 </Table>
             </Row>
 
             <Row className={"justify-content-md-center"}>
-                    <Button variant="secondary" onClick={onAddItem} className={"add-item-button"}>
-                        &#x2B; Add Item
-                    </Button>
+                <Button variant="secondary" onClick={onAddItem} className={"add-item-button"}>
+                    &#x2B; Add Item
+                </Button>
 
-                    <Button onClick={() => {onSubmit(elementsProp)}} className={"submit-btn"}>
-                        Submit
-                    </Button>
+                <Button onClick={() => {
+                    if (getTotalProportion() === 100){
+                        onSubmit(elementsProp);
+                    } else {
+                        setShowProportionWarning(true);
+                    }
+                }} className={"submit-btn"}>
+                    Submit
+                </Button>
             </Row>
 
         </div>
@@ -151,8 +183,8 @@ export default function Homepage() {
             state: {
                 ids: ids,
                 proportion: proportions,
-                start_timestamp: + startDate,
-                end_timestamp: + endDate
+                start_timestamp: +startDate,
+                end_timestamp: +endDate
             }
         })
     }
@@ -162,19 +194,19 @@ export default function Homepage() {
                         spinner={<PropagateLoader color={"#36D7B7"}/>}
         >
             <Container style={{padding: "1rem 0", minHeight: '100vh'}}>
-                <Row >
+                <Row>
                     <Row className={"pt-2"}>
                         <Col className={"display-flex-center"}>
                             <p className={"date-label"}>Start Date: </p>
                             <div className={"width-200"}>
-                                <DatePicker selected={startDate} onChange={(date) => setStartDate(date)} />
+                                <DatePicker selected={startDate} onChange={(date) => setStartDate(date)}/>
                             </div>
                         </Col>
 
                         <Col className={"display-flex-center"}>
                             <p className={"date-label"}>End Date: </p>
                             <div className={"width-200"}>
-                                <DatePicker selected={endDate} onChange={(date) => setEndDate(date)} />
+                                <DatePicker selected={endDate} onChange={(date) => setEndDate(date)}/>
                             </div>
                         </Col>
                     </Row>
